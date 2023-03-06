8041fc85f10580323dc69efcd7e0d2fa48b1e445
Carlos Daniel <carlosdaniel@Mac-mini-de-Carlos.local>
2023-03-06 14:53:04 -0300

    finalização do projeto

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function timerConversion(duration, displayMinutes, displaySeconds) {
+function timerConversion(duration, displayMinutes, displaySeconds, rocket) {
+    const audio = new Audio("songAlert.mp3");
     let timer = duration, minutes, seconds;
     let myInterval = setInterval(() => {
         minutes = parseInt(timer / 60, 10);
@@ -9,16 +10,47 @@ function timerConversion(duration, displayMinutes, displaySeconds) {
         displaySeconds.textContent = seconds;
         if (--timer < 0) {
             timer = 0;
-            console.log("acabou");
+            audio.play();
+            rocket.style.animation = "rocketUp 6s";
             clearInterval(myInterval);
         }
     }, 1000);
 }
 function startCountdown() {
-    let timeCountdown = document.getElementById("timeCountdown").value;
+    let messageError = document.getElementById("messageError");
+    let inputValue = document.getElementById("timerValue");
+    let countdown = document.getElementById("countdownContainer");
+    let formTimer = document.getElementById("formTimerValue");
+    let rocket = document.getElementById("rocket");
+    let btnStart = document.getElementById("btnStart");
+    let timeCountdown = document.getElementById("timerValue").value;
     let timerCountdownConversion = parseInt(timeCountdown);
-    let duration = 60 * timerCountdownConversion; // Converter para segundos
-    displayMinutes = document.querySelector('#countdownMin'); // selecionando o timer
-    displaySeconds = document.querySelector('#countdownSec'); // selecionando o timer
-    timerConversion(duration, displayMinutes, displaySeconds); // iniciando o timer
+
+    if (timerCountdownConversion <= 0) {
+        messageError.style.display = "block";
+        messageError.style.animation = "mooveError 0.2s";
+        inputValue.style.border = "1px solid red";
+        inputValue.style.animation = "mooveError 0.2s";
+    } else if (timerCountdownConversion > 50) {
+        messageError.style.display = "block";
+        messageError.style.animation = "mooveError 0.2s";
+        inputValue.style.border = "1px solid red";
+        inputValue.style.animation = "mooveError 0.2s";
+    } else {
+        btnStart.style.backgroundColor = "rgba(255, 255, 255, 0.25)";
+        let viewTimerConfig = setTimeout(() => {
+            formTimer.style.display = "none";
+            countdown.style.display = "flex";
+            rocket.style.animation = "mooveInfinit 3s infinite";
+        }, 1000)
+
+        let duration = 60 * timerCountdownConversion; // Converter para segundos
+        displayMinutes = document.querySelector('#countdownMin'); // selecionando o timer
+        displaySeconds = document.querySelector('#countdownSec'); // selecionando o timer
+        timerConversion(duration, displayMinutes, displaySeconds, rocket); // iniciando o timer
+    }
 };
+
+function newTimerValue() {
+    window.location.href = "index.html";
+}
